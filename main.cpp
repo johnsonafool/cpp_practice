0f183c4242eef8a1115b689a3418097bcbefa6d9
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-07-10 11:43:13 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    
+    
+//    std::cout << "Enter two numbers:" << std::endl; int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    std::cout << "The sum of " << v1 << " and " << v2
+//                 << " is " << v1 + v2 << std::endl;
+//    std::cout << "The sum of " << v1 << " and " << v2
+//              << " is " << v1 * v2 << std::endl;
+    
+//    std::cout << "hello, world" << std::endl;
+//    std::cout << "Enter two numbers:" << std::endl; int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    int val = v1;
+//    while (val < v2 -1) {
+//        std::cout << val +1 << std::endl;
+//        ++val;
+//
+//    }
+    int sum = 0;
+       for (int i = -100; i <= 100; ++i)
+           sum += i; // guess 0
+    std::cout << sum;
+    
+    return 0;
+}
+
+
